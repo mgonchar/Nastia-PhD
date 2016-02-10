03358659d38f655bf4f933e9f2a4c7e0c53fb2dc
mgonchar <goncharenko.mir@yandex.ru>
2016-02-11 00:34:54 +0300

    Updated words list; reworked words arrays randomization

--- shuffle_words.py
+++ shuffle_words.py
@@ -11,7 +11,7 @@ def invert_condition(cnd):
         's' : 'h',
         'h' : 's'
     }.get(cnd)
-    
+
 def create_cur_sb_array(arm_words, leg_words, nouns, sb_order):
     a_i = l_i = n_i = 0
     current_words = []
@@ -26,7 +26,7 @@ def create_cur_sb_array(arm_words, leg_words, nouns, sb_order):
             current_words += nouns[n_i:n_i+4]
             n_i += 4
     file = open("./words.tem",'wb')
-    
+
     s = "array {"
     file.write(s.encode("UTF-8"))
     for caption in current_words:
@@ -35,7 +35,7 @@ def create_cur_sb_array(arm_words, leg_words, nouns, sb_order):
     s = "}word_array;"
     file.write(s.encode("UTF-8"))
     file.close()
-        
+
 import random, subprocess, sys, os
 
 al         = ['a','l']
@@ -48,83 +48,118 @@ random.shuffle(difficulty)
 
 current_design.append((al[0] + seq_type[0] + difficulty[0]))
 for i in range(1,8):
-    
+
     dead_end = (al[i%2] + invert_condition(current_design[i-1][1]) + current_design[i-1][2] in current_design) and \
     (al[i%2] + current_design[i-1][1] + invert_condition(current_design[i-1][2]) in current_design)
-    
+
     if dead_end:
         new_val = al[i%2] + current_design[i-1][1] + current_design[i-1][2]
         if new_val in current_design:
             print("This should never happen")
             break
-    else:               
+    else:
         while True:
             if random.randint(0,1) == 0:
                 new_val = al[i%2] + invert_condition(current_design[i-1][1]) + current_design[i-1][2]
             else:
                 new_val = al[i%2] + current_design[i-1][1] + invert_condition(current_design[i-1][2])
-                
+
             if not new_val in current_design:
                 break
-            
+
     current_design.append(new_val)
 
 current_design.insert(4,'n')
-#print("experiment scenaries initialized\n")    
+#print("experiment scenaries initialized\n")
 #print(*current_design, sep='\n')
 
-arm_words = ["написать","держать","положить","касаться","подписать","поднимать","бросать","ловить","рисовать","класть","трогать","тащить","печатать","резать","хлопать","вырезать","гладить","рубить","пожимать","кидать","отрывать","обхватить","нажимать","тереть", \
-"вязать","швырять","ударять","черпать","мазать","ощупывать","царапать","чертить","скрести","щупать","взломать","рвать"]
+# arm_words = ["написать","держать","положить","касаться","подписать","поднимать","бросать","ловить","рисовать","класть","трогать","тащить","печатать","резать","хлопать","вырезать","гладить","рубить","пожимать","кидать","отрывать","обхватить","нажимать","тереть", \
+# "вязать","швырять","ударять","черпать","мазать","ощупывать","царапать","чертить","скрести","щупать","взломать","рвать"]
+#
+# leg_words = ["ходить","приходить","уходить","бежать","вставать","бегать","сходить","наступать","рысить","прыгать","бродить","убежать","шагать","отходить","ступать","обходить","прибегать","скользить","скакать","прибежать","брести","топтаться","топать","топтать", \
+# "плестись","хромать","шаркать","забрести","приседать","пинать","семенить","оступиться","запрыгнуть","отдавить","втоптать","лягать"]
+#
+# nouns = ["дорога","картина","бумага","фотография","больница","стакан","двигатель","краска","подъезд","бассейн","курица","асфальт","молния","снимок","капуста","бабочка","украшение","упаковка","обложка","троллейбус","лезвие","зажигалка","пшеница","штукатурка","кукушка",\
+# "штанга","котлован","упряжка","пастбище","кружево","развилка","чернослив","кострище","аббатство","грифель","фабрика"]
 
-leg_words = ["ходить","приходить","уходить","бежать","вставать","бегать","сходить","наступать","рысить","прыгать","бродить","убежать","шагать","отходить","ступать","обходить","прибегать","скользить","скакать","прибежать","брести","топтаться","топать","топтать", \
-"плестись","хромать","шаркать","забрести","приседать","пинать","семенить","оступиться","запрыгнуть","отдавить","втоптать","лягать"]
+arm_words = ["написать","держать","касаться","подписать","бросать ","ловить","рисовать","класть","трогать","тащить",\
+            "резать","хлопать","вырезать","гладить","кидать","отрывать","нажимать","тереть","вязать","швырять","ударять",\
+            "черпать","мазать","царапать","чертить","скрести","щупать","взломать"]
 
-nouns = ["дорога","картина","бумага","фотография","больница","стакан","двигатель","краска","подъезд","бассейн","курица","асфальт","молния","снимок","капуста","бабочка","украшение","упаковка","обложка","троллейбус","лезвие","зажигалка","пшеница","штукатурка","кукушка",\
-"штанга","котлован","упряжка","пастбище","кружево","развилка","чернослив","кострище","аббатство","грифель","фабрика"]
+leg_words = ["ходить","уходить","бежать","вставать","бегать","сходить","наступать","рысить","прыгать","бродить","убежать",\
+            "шагать","отходить","ступать","обходить","скакать","брести","топать","топтать","плестись","хромать","шаркать",\
+            "забрести","пинать","семенить","отдавить","втоптать","лягать"]
+
+nouns = ["дорога","картина","бумага","больница","стакан","краска","бассейн","курица","молния","снимок","капуста","бабочка",\
+        "упаковка","обложка","лезвие","зажигалка","пшеница","штанга","котлован","упряжка","пастбище","кружево","развилка",\
+        "кострище","грифель","отвертка","духовка","парусник"]
 
 total_arm_words = []
 total_leg_words = []
 total_nouns = []
-for i in range(0,12):
-    random.shuffle(arm_words)    
-    total_arm_words += arm_words 
-    random.shuffle(leg_words)
-    total_leg_words += leg_words  
-    random.shuffle(nouns)
-    total_nouns     += nouns 
+for i in range(0,16):
+    tmp = arm_words[0:12]
+    random.shuffle(tmp)
+    total_arm_words += tmp
+    tmp = arm_words[12:24]
+    random.shuffle(tmp)
+    total_arm_words += tmp
+    tmp = arm_words[24:]
+    random.shuffle(tmp)
+    total_arm_words += tmp
 
-#print("\narmwords:\n")    
+    tmp = leg_words[0:12]
+    random.shuffle(tmp)
+    total_leg_words += tmp
+    tmp = leg_words[12:24]
+    random.shuffle(tmp)
+    total_leg_words += tmp
+    tmp = leg_words[24:]
+    random.shuffle(tmp)
+    total_leg_words += tmp
+
+    tmp = nouns[0:12]
+    random.shuffle(tmp)
+    total_nouns += tmp
+    tmp = nouns[12:24]
+    random.shuffle(tmp)
+    total_nouns += tmp
+    tmp = nouns[24:]
+    random.shuffle(tmp)
+    total_nouns += tmp
+
+#print("\narmwords:\n")
 #print(*total_arm_words, sep='\n')
 
-total_experiment_sb_order = [[0 for x in range(36)] for x in range(9)] #create 2d array for holding total seq of sb 
+total_experiment_sb_order = [[0 for x in range(36)] for x in range(9)] #create 2d array for holding total seq of sb
 
 for i in range(0,9):
     sb_types = ['a','l','n']
     for k in range(0,11):
         sb_types += ['a','l','n']
-    
+
     random.shuffle(sb_types)
-    
+
     total_experiment_sb_order[i][0] = sb_types.pop(random.randint(0,len(sb_types)-1))
     total_experiment_sb_order[i][1] = sb_types.pop(random.randint(0,len(sb_types)-1))
     total_experiment_sb_order[i][2] = sb_types.pop(random.randint(0,len(sb_types)-1))
-    
+
     #for j in range(3,36):
     j = 3
     dead_end = False
     while len(sb_types) != 0:
         if not dead_end:
-            while True: 
+            while True:
                 idx = random.randint(0,len(sb_types)-1)
                 tmp_sb_type = sb_types[idx]
-                if not (tmp_sb_type == total_experiment_sb_order[i][j-1] and tmp_sb_type == total_experiment_sb_order[i][j-2] and 
+                if not (tmp_sb_type == total_experiment_sb_order[i][j-1] and tmp_sb_type == total_experiment_sb_order[i][j-2] and
                         tmp_sb_type == total_experiment_sb_order[i][j-2]):
                     sb_types.pop(idx)
                     total_experiment_sb_order[i][j] = tmp_sb_type
                     j+=1
                     break
                 else:
-                    dead_end = True                    
+                    dead_end = True
                     tmp_sb_type = sb_types[0]
                     for cur_sb in sb_types[1:]:
                         dead_end = dead_end and cur_sb == tmp_sb_type
@@ -139,12 +174,12 @@ for i in range(0,9):
                     else:
                         total_experiment_sb_order[i].insert(idx,sb_types.pop(0))
                         j+=1
-                        break 
+                        break
             list(filter((0).__ne__, total_experiment_sb_order[i])) # delete redundant zeros from initialization
-        
+
 path_to_experiment_folder = "C:\\Documents and Settings\\Admin\\Рабочий стол\\Experiments\\Nastia PhD"
 path_to_presentation_launcher = "C:\\Program Files\\Neurobehavioral Systems\\Presentation\\Version172100814\\PresentationLauncher.exe"
-subject_id = sys.argv[1] #"subject_id" 
+subject_id = sys.argv[1] #"subject_id"
 
 if not os.path.exists(path_to_experiment_folder+'/logs/'+subject_id):
 	os.makedirs(path_to_experiment_folder+'/logs/'+subject_id)
@@ -153,24 +188,24 @@ if not os.path.exists(path_to_experiment_folder+'/recorded_responces/'+subject_i
 	os.makedirs(path_to_experiment_folder+'/recorded_responces/'+subject_id)
 
 #print("Start forming function calls...\n")
-for i in range(0,9): 
+for i in range(0,9):
     create_cur_sb_array(total_arm_words[48*i:48*(i+1)], total_leg_words[48*i:48*(i+1)], total_nouns[48*i:48*(i+1)], total_experiment_sb_order[i])
     if current_design[i] != 'n':
         cur_al = {
             'a' : "arm",
             'l' : "leg"
         }.get(current_design[i][0])
-        
+
         cur_st = {
             '1' : "on_input",
             '2' : "on_pause"
         }.get(current_design[i][1])
-        
+
         cur_df = {
             's' : "simple",
             'h' : "hard"
         }.get(current_design[i][2])
-        
+
         subprocess.call([path_to_presentation_launcher, '-e',path_to_experiment_folder+'\\testo.exp', '-s', path_to_experiment_folder+'\\'+cur_al+'_'+cur_st+'_'+cur_df+'_series.sce', '-n', subject_id, '-o','-l', path_to_experiment_folder+'/logs/'+
                         subject_id+'/'+subject_id+'_'+current_design[i]+'.log'])
         #print(path_to_presentation_launcher+ ' -s ' + cur_al+'_'+cur_st+'_'+cur_df+'_series.sce '+ ' -n '+ subject_id+ ' -l '+ path_to_experiment_folder+'/'+
@@ -180,10 +215,10 @@ for i in range(0,9):
                         subject_id+'/'+subject_id+'_'+current_design[i]+'.log'])
         #print(path_to_presentation_launcher+ ' -s '+ 'no_action_series.sce'+ ' -n '+ subject_id+ ' -l '+ path_to_experient_folder+'/'+
         #                subject_id+'/'+subject_id+'_'+current_design[i]+'.log')
-      
+
 
 #    subprocess.call([path_to_presentation_launcher, '-e',path_to_experiment_folder+'\\testo.exp','-s', path_to_experiment_folder+'\\arm_on_input_simple_series.sce', '-n', subject_id, '-o','-l', path_to_experiment_folder+'\\logs\\'+
 #							subject_id+'\\'+subject_id+'_a1s.log'])
 #print(path_to_presentation_launcher, '-e',"'"+path_to_experiment_folder+'/testo.exp'+"'",'-s', "'"+path_to_experiment_folder+'/'+'arm_on_input_simple_series.sce'+"'", '-n', subject_id, '-o','-l', "'"+path_to_experiment_folder+'/'+
-#                        subject_id+'/'+subject_id+'_a1s.log'+"'")  
+#                        subject_id+'/'+subject_id+'_a1s.log'+"'")
 os.remove("./words.tem")
--- shuffle_words_training.py
+++ shuffle_words_training.py
@@ -11,7 +11,7 @@ def invert_condition(cnd):
         's' : 'h',
         'h' : 's'
     }.get(cnd)
-    
+
 def create_cur_sb_array(arm_words, leg_words, nouns, sb_order):
     a_i = l_i = n_i = 0
     current_words = []
@@ -26,7 +26,7 @@ def create_cur_sb_array(arm_words, leg_words, nouns, sb_order):
             current_words += nouns[n_i:n_i+4]
             n_i += 4
     file = open("./words.tem",'wb')
-    
+
     s = "array {"
     file.write(s.encode("UTF-8"))
     for caption in current_words:
@@ -35,7 +35,7 @@ def create_cur_sb_array(arm_words, leg_words, nouns, sb_order):
     s = "}word_array;"
     file.write(s.encode("UTF-8"))
     file.close()
-        
+
 import random, subprocess, sys, os
 
 al         = ['a','l']
@@ -48,83 +48,115 @@ random.shuffle(difficulty)
 
 current_design.append((al[0] + seq_type[0] + difficulty[0]))
 for i in range(1,8):
-    
+
     dead_end = (al[i%2] + invert_condition(current_design[i-1][1]) + current_design[i-1][2] in current_design) and \
     (al[i%2] + current_design[i-1][1] + invert_condition(current_design[i-1][2]) in current_design)
-    
+
     if dead_end:
         new_val = al[i%2] + current_design[i-1][1] + current_design[i-1][2]
         if new_val in current_design:
             print("This should never happen")
             break
-    else:               
+    else:
         while True:
             if random.randint(0,1) == 0:
                 new_val = al[i%2] + invert_condition(current_design[i-1][1]) + current_design[i-1][2]
             else:
                 new_val = al[i%2] + current_design[i-1][1] + invert_condition(current_design[i-1][2])
-                
+
             if not new_val in current_design:
                 break
-            
+
     current_design.append(new_val)
 
 current_design.insert(4,'n')
-#print("experiment scenaries initialized\n")    
+#print("experiment scenaries initialized\n")
 #print(*current_design, sep='\n')
 
-arm_words = ["подумать", "провести", "получать", "мешать", "указать", "явиться", "напомнить", "защищать", "приглашать", "стучать", "прятать", "ругать", "наказать", "прикрывать", "сжимать", "увлечь", "выучить", "скрипеть", "набраться", "накормить", "угощать", \
-"уделить", "сломаться", "косить", "смешать", "взойти", "винить", "одевать", "целиться", "метить", "взбить", "чертить", "загнуть", "молоть", "взмахивать"]
+# arm_words = ["подумать", "провести", "получать", "мешать", "указать", "явиться", "напомнить", "защищать", "приглашать", "стучать", "прятать", "ругать", "наказать", "прикрывать", "сжимать", "увлечь", "выучить", "скрипеть", "набраться", "накормить", "угощать", \
+# "уделить", "сломаться", "косить", "смешать", "взойти", "винить", "одевать", "целиться", "метить", "взбить", "чертить", "загнуть", "молоть", "взмахивать"]
+#
+# leg_words = ["читать", "искать", "передать", "обладать", "определять", "находить", "доказать", "выпускать", "владеть", "покачать", "трогать", "мыслить", "вкладывать", "обвинить", "раскрывать", "усилить", "твердить", "смущать", "путать", "гонять", "развить", \
+# "утешать", "вешать", "расширять", "посмеяться", "тосковать", "возмутить", "задевать", "растаять", "спутать", "чинить", "клевать", "вынуждать", "залечь", "засверкать"]
+#
+# nouns = ["комната", "директор", "звезда", "бутылка", "кресло", "лестница", "колесо", "публикация", "бассейн", "сиденье", "учебник", "конверт", "гвоздь", "таблетка", "конфета", "простыня", "папироса", "грамота", "упаковка", "занавеска", "раковина", "отпечаток", \
+# "пружина", "кошелек", "клумба", "духовка", "глобус", "баррикада", "капюшон", "компрессор", "гавань", "застежка", "парусник", "этажерка", "отвертка"]
 
-leg_words = ["читать", "искать", "передать", "обладать", "определять", "находить", "доказать", "выпускать", "владеть", "покачать", "трогать", "мыслить", "вкладывать", "обвинить", "раскрывать", "усилить", "твердить", "смущать", "путать", "гонять", "развить", \
-"утешать", "вешать", "расширять", "посмеяться", "тосковать", "возмутить", "задевать", "растаять", "спутать", "чинить", "клевать", "вынуждать", "залечь", "засверкать"]
+arm_words = ["ругать","увлечь","косить","взойти","винить","метить","взбить","молоть","указать","стучать","прятать","сжимать","выучить","угощать",\
+             "уделить","смешать","одевать","чертить","загнуть","подумать","провести","получать","защищать","наказать","скрипеть","целиться","оплатить","изобрести"]
 
-nouns = ["комната", "директор", "звезда", "бутылка", "кресло", "лестница", "колесо", "публикация", "бассейн", "сиденье", "учебник", "конверт", "гвоздь", "таблетка", "конфета", "простыня", "папироса", "грамота", "упаковка", "занавеска", "раковина", "отпечаток", \
-"пружина", "кошелек", "клумба", "духовка", "глобус", "баррикада", "капюшон", "компрессор", "гавань", "застежка", "парусник", "этажерка", "отвертка"]
+leg_words = ["читать","искать","путать","гонять","вешать","отмыть","залечь","раздуть","утолить","владеть","мыслить","усилить","смущать","развить",\
+            "утешать","спутать","клевать","обнести","вертеть","обладать","находить","доказать","покачать","обвинить","твердить","задевать","растаять","выпускать"]
+
+nouns = ["куртка","кресло","гавань","колесо","клумба","звезда","глобус","гвоздь","гавань","учебник","сиденье","пружина","кошелек","конфета","конверт",\
+         "комната","капюшон","бутылка","бассейн","этажерка","упаковка","таблетка","раковина","простыня","папироса","лестница","застежка","занавеска"]
 
 total_arm_words = []
 total_leg_words = []
 total_nouns = []
-for i in range(0,4):
-    random.shuffle(arm_words)    
-    total_arm_words += arm_words 
-    random.shuffle(leg_words)
-    total_leg_words += leg_words  
-    random.shuffle(nouns)
-    total_nouns     += nouns 
+for i in range(0,7):
+    tmp = arm_words[0:12]
+    random.shuffle(tmp)
+    total_arm_words += tmp
+    tmp = arm_words[12:24]
+    random.shuffle(tmp)
+    total_arm_words += tmp
+    tmp = arm_words[24:]
+    random.shuffle(tmp)
+    total_arm_words += tmp
 
-#print("\narmwords:\n")    
+    tmp = leg_words[0:12]
+    random.shuffle(tmp)
+    total_leg_words += tmp
+    tmp = leg_words[12:24]
+    random.shuffle(tmp)
+    total_leg_words += tmp
+    tmp = leg_words[24:]
+    random.shuffle(tmp)
+    total_leg_words += tmp
+
+    tmp = nouns[0:12]
+    random.shuffle(tmp)
+    total_nouns += tmp
+    tmp = nouns[12:24]
+    random.shuffle(tmp)
+    total_nouns += tmp
+    tmp = nouns[24:]
+    random.shuffle(tmp)
+    total_nouns += tmp
+
+#print("\narmwords:\n")
 #print(*total_arm_words, sep='\n')
 
-total_experiment_sb_order = [[0 for x in range(12)] for x in range(9)] #create 2d array for holding total seq of sb 
+total_experiment_sb_order = [[0 for x in range(12)] for x in range(9)] #create 2d array for holding total seq of sb
 
 for i in range(0,9):
     sb_types = ['a','l','n']
     for k in range(0,3):
         sb_types += ['a','l','n']
-    
+
     random.shuffle(sb_types)
-    
+
     total_experiment_sb_order[i][0] = sb_types.pop(random.randint(0,len(sb_types)-1))
     total_experiment_sb_order[i][1] = sb_types.pop(random.randint(0,len(sb_types)-1))
     total_experiment_sb_order[i][2] = sb_types.pop(random.randint(0,len(sb_types)-1))
-    
+
     #for j in range(3,36):
     j = 3
     dead_end = False
     while len(sb_types) != 0:
         if not dead_end:
-            while True: 
+            while True:
                 idx = random.randint(0,len(sb_types)-1)
                 tmp_sb_type = sb_types[idx]
-                if not (tmp_sb_type == total_experiment_sb_order[i][j-1] and tmp_sb_type == total_experiment_sb_order[i][j-2] and 
+                if not (tmp_sb_type == total_experiment_sb_order[i][j-1] and tmp_sb_type == total_experiment_sb_order[i][j-2] and
                         tmp_sb_type == total_experiment_sb_order[i][j-2]):
                     sb_types.pop(idx)
                     total_experiment_sb_order[i][j] = tmp_sb_type
                     j+=1
                     break
                 else:
-                    dead_end = True                    
+                    dead_end = True
                     tmp_sb_type = sb_types[0]
                     for cur_sb in sb_types[1:]:
                         dead_end = dead_end and cur_sb == tmp_sb_type
@@ -139,12 +171,12 @@ for i in range(0,9):
                     else:
                         total_experiment_sb_order[i].insert(idx,sb_types.pop(0))
                         j+=1
-                        break 
+                        break
             list(filter((0).__ne__, total_experiment_sb_order[i])) # delete redundant zeros from initialization
-        
+
 path_to_experiment_folder = "C:\\Documents and Settings\\Admin\\Рабочий стол\\Experiments\\Nastia PhD"
 path_to_presentation_launcher = "C:\\Program Files\\Neurobehavioral Systems\\Presentation\\Version172100814\\PresentationLauncher.exe"
-subject_id = sys.argv[1] #"subject_id" 
+subject_id = sys.argv[1] #"subject_id"
 
 if not os.path.exists(path_to_experiment_folder+'/logs/'+subject_id):
 	os.makedirs(path_to_experiment_folder+'/logs/'+subject_id)
@@ -153,24 +185,24 @@ if not os.path.exists(path_to_experiment_folder+'/recorded_responces/'+subject_i
 	os.makedirs(path_to_experiment_folder+'/recorded_responces/'+subject_id)
 
 #print("Start forming function calls...\n")
-for i in range(0,9): 
+for i in range(0,9):
     create_cur_sb_array(total_arm_words[16*i:16*(i+1)], total_leg_words[16*i:16*(i+1)], total_nouns[16*i:16*(i+1)], total_experiment_sb_order[i])
     if current_design[i] != 'n':
         cur_al = {
             'a' : "arm",
             'l' : "leg"
         }.get(current_design[i][0])
-        
+
         cur_st = {
             '1' : "on_input",
             '2' : "on_pause"
         }.get(current_design[i][1])
-        
+
         cur_df = {
             's' : "simple",
             'h' : "hard"
         }.get(current_design[i][2])
-        
+
         subprocess.call([path_to_presentation_launcher, '-e',path_to_experiment_folder+'\\testo.exp', '-s', path_to_experiment_folder+'\\'+cur_al+'_'+cur_st+'_'+cur_df+'_series.sce', '-n', subject_id, '-o','-l', path_to_experiment_folder+'/logs/'+
                         subject_id+'/'+subject_id+'_'+current_design[i]+'.log'])
         #print(path_to_presentation_launcher+ ' -s ' + cur_al+'_'+cur_st+'_'+cur_df+'_series.sce '+ ' -n '+ subject_id+ ' -l '+ path_to_experiment_folder+'/'+
@@ -180,10 +212,10 @@ for i in range(0,9):
                         subject_id+'/'+subject_id+'_'+current_design[i]+'.log'])
         #print(path_to_presentation_launcher+ ' -s '+ 'no_action_series.sce'+ ' -n '+ subject_id+ ' -l '+ path_to_experient_folder+'/'+
         #                subject_id+'/'+subject_id+'_'+current_design[i]+'.log')
-      
+
 
 #    subprocess.call([path_to_presentation_launcher, '-e',path_to_experiment_folder+'\\testo.exp','-s', path_to_experiment_folder+'\\arm_on_input_simple_series.sce', '-n', subject_id, '-o','-l', path_to_experiment_folder+'\\logs\\'+
 #							subject_id+'\\'+subject_id+'_a1s.log'])
 #print(path_to_presentation_launcher, '-e',"'"+path_to_experiment_folder+'/testo.exp'+"'",'-s', "'"+path_to_experiment_folder+'/'+'arm_on_input_simple_series.sce'+"'", '-n', subject_id, '-o','-l', "'"+path_to_experiment_folder+'/'+
-#                        subject_id+'/'+subject_id+'_a1s.log'+"'")  
+#                        subject_id+'/'+subject_id+'_a1s.log'+"'")
 os.remove("./words.tem")
